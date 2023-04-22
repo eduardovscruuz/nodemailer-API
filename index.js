7fe443a3f2510d5db2fd39e3864fc748f7f1e35e
eduardovscruuz <eduardovscruuz@gmail.com>
2023-04-21 21:32:45 -0300

    crossorigin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');
+  res.header('Access-Control-Allow-Origin', 'https://eduardovscruuz.github.io/portfolio/');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
